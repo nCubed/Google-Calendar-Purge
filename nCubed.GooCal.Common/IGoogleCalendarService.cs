97f5acc520047b02cd9b7d01a174df049ea513b4
Patrick <ppalms@outlook.com>
2014-06-04 18:09:28 -0500

    Added IGoogleCalendarService interface
    Trying to unit test. Derp.

---
+++ nCubed.GooCal.Common/IGoogleCalendarService.cs
@@ -0,0 +1,31 @@
+﻿using Google.GData.Calendar;
+using Google.GData.Client;
+
+namespace nCubed.GooCal.Common
+{
+    /// <summary>
+    /// Adapter for the Google <see cref="CalendarService"/> class
+    /// </summary>
+    public interface IGoogleCalendarService
+    {
+        // ReSharper disable once InconsistentNaming
+        /// <summary>
+        /// Sets the user credentials.
+        /// </summary>
+        /// <param name="userName">The Google username.</param>
+        /// <param name="password">The user's password.</param>
+        void setUserCredentials( string userName, string password );
+
+        /// <summary>
+        /// Queries the Google Calendar.
+        /// </summary>
+        /// <param name="feedQuery">The event query.</param>
+        EventFeed Query( EventQuery feedQuery );
+
+        /// <summary>
+        /// Queries the Google Calendar.
+        /// </summary>
+        /// <param name="feedQuery">The feed query.</param>
+        AtomFeed Query( FeedQuery feedQuery );
+    }
+}
